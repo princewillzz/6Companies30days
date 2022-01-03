a0751f706435889fc53a04e57b2984c608aa7218
princewillzz <princewillz2013@gmail.com>
2022-01-03 21:31:27 +0530

    encoded string space optimum

--- Goldman/EncodeString.java
+++ Goldman/EncodeString.java
@@ -16,6 +16,13 @@ class EncodedString {
             GfG g = new GfG();
             System.out.println(g.encode(str));
 
+            // Space optimum
+            char chars[] = str.toCharArray();
+            int length = g.encode2(chars);
+            for (int i = 0; i < length; i++) {
+                System.out.print(chars[i]);
+            } System.out.println();
+
             T--;
         }
     }
@@ -36,10 +43,38 @@ class GfG {
                 index++;
             }
 
-            encoded.append(ch).append(count);
+            encoded.append(ch);
+            if(count > 1)
+                encoded.append(count);
         }
 
         return encoded.toString();
+    }
+
+    // Space optimised
+    int encode2(char chars[]) {
+
+        int indexToEdit = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            char ch = chars[i];
+            int countCh = 1;
+
+            while (i+1 < chars.length && ch == chars[i+1]) {
+                countCh++;
+                i++;
+            }
+
+            chars[indexToEdit++] = ch;
+            if(countCh > 1) {
+                String countString = String.valueOf(countCh);
+                for (int j = 0; j < countString.length(); j++) {
+                    chars[indexToEdit++] = countString.charAt(j);
+                }
+            }
+        }
+
+        return indexToEdit;
     }
 
 }
