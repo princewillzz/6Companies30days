903dbaa790d7e9795746540f4afa95533feb674c
princewillzz <princewillz2013@gmail.com>
2022-01-11 18:57:47 +0530

    col name

---
+++ Amazon/ColumnName.java
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            long n = sc.nextLong();
+    		System.out.println (new Solution().colName (n));
+        }
+        
+    }
+}
+
+// Contributed By: Pranay Bansal 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    String colName (long n)
+    {
+        StringBuffer sb=new StringBuffer();
+        while(n>0){
+            char ch = 'Z';
+            if(n%26 != 0) ch = (char) (64+(n%26));
+            
+            sb.append(ch);
+            n=(n-1)/26;
+        }
+        sb.reverse();
+        return(sb.toString());
+    }
+}
